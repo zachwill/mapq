bb94c636a4140408fcc59b415f0cbd46d8b21724
Zach Williams <hey@zachwill.com>
2012-03-12 07:07:23 -0700

    Should be able to pass in keyword arguments

--- mapq/core.py
+++ mapq/core.py
@@ -5,26 +5,26 @@ Simplified methods for calling Mapquest's Geocoding API.
 from .geo import Geo
 
 
-def address(name):
+def address(name, **kwargs):
     """Geocode an address."""
-    return Geo().address(name)
+    return Geo().address(name, **kwargs)
 
 
-def batch(*locations):
+def batch(*locations, **kwargs):
     """Batch geocode multiple addresses."""
-    return Geo().batch(*locations)
+    return Geo().batch(*locations, **kwargs)
 
 
-def geocode(address):
+def geocode(address, **kwargs):
     """Return first the geocode result for a given address."""
-    return Geo().address(address)[0]
+    return Geo().address(address, **kwargs)[0]
 
 
-def latlng(address):
+def latlng(address, **kwargs):
     """Get the latitude and longitude coordinates for an address."""
-    return Geo().latlng(address)
+    return Geo().latlng(address, **kwargs)
 
 
-def reverse(lat, lng):
+def reverse(lat, lng, **kwargs):
     """Reverse geocode lat/lng coordinates."""
-    return Geo().reverse(lat, lng)
+    return Geo().reverse(lat, lng, **kwargs)
